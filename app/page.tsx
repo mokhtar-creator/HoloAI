50582ddccf5ed3979737ac627fbf19ef6578c6cc
mokhtar-creator <elgintilana@gmail.com>
2025-12-06 01:52:29 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hologram from "@/components/Hologram"
+import ChatHUD from "@/components/ChatHUD"
+
+export default function Page(){
+  return (
+    <main className="container">
+      <h1 style={{fontSize:28, marginBottom:12}}>HoloAI</h1>
+      <div className="row">
+        <Hologram />
+        <ChatHUD />
+      </div>
+      <p style={{opacity:.7, marginTop:16}}>ضع avatar.mp4 داخل مجلد public</p>
+    </main>
+  )
+}
